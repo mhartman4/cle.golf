ad60615cc9c366a4a792a763cb318e14d45033c8
mhartman4 <mhartman4@gmail.com>
2018-05-22 11:03:29 -0400

    initial commit!

---
+++ TEAM EVENT.js
@@ -0,0 +1,379 @@
+teams = {
+  "J Mike Hates DV": ["Rickie Fowler" ,"Jon Rahm" ,"Smylie Kaufman" ,"Sangmoon Bae" ,"Michael Kim" ,"Andrew Loupe" ,"Michael Bradley" ,"Jonathan Byrd" ,"Jason Day" ,"Ollie Schniederjans" ,"Peter Uihlein" ,"Aaron Wise" ,"Beau Hossler" ,"Xander Schauffele" ,"Nick Taylor" ,"Ryan Blaum"],
+"High Indecision": ["Dustin Johnson" ,"Russell Henley" ,"Patrick Cantlay" ,"Patton Kizzire" ,"Peter Uihlein" ,"Andrew Landry" ,"Stephan Jaeger" ,"Sam Ryder" ,"Kevin Na" ,"Keegan Bradley" ,"Bryson DeChambeau" ,"Branden Grace" ,"Jamie Lovemark" ,"Danny Willett" ,"Martin Laird" ,"Daniel Summerhays"],
+"Easy Ed`s Paralegals": ["Justin Thomas" ,"Paul Casey" ,"Jamie Lovemark" ,"Harold Varner III" ,"Peter Uihlein" ,"Tiger Woods" ,"Ryan Armour" ,"Miguel Angel Carballo" ,"Hideki Matsuyama" ,"Rickie Fowler" ,"Patrick Cantlay" ,"Richy Werenski" ,"Bud Cauley" ,"Andrew Landry" ,"Stephan Jaeger" ,"Sam Ryder"],
+"Wally's Hole": ["Justin Thomas" ,"Charley Hoffman" ,"Tommy Fleetwood" ,"Tyrrell Hatton" ,"Bernd Wiesberger" ,"Ross Fisher" ,"Trevor Immelman" ,"Tiger Woods" ,"Jason Day" ,"Brendan Steele" ,"Tony Finau" ,"Graham DeLaet" ,"Cameron Percy" ,"Bud Cauley" ,"Thomas Bjorn" ,"Andrew Landry"],
+"Putt Putting Bridesmaids": ["Rickie Fowler" ,"Jon Rahm" ,"Patrick Cantlay" ,"Thomas Pieters" ,"Bud Cauley" ,"Aaron Wise" ,"Beau Hossler" ,"Austin Cook" ,"Webb Simpson" ,"Tony Finau" ,"Kevin Na" ,"Jamie Lovemark" ,"Sangmoon Bae" ,"Patrick Rodgers" ,"Chesson Hadley" ,"Sam Saunders"],
+"Guardians of the Fig Newtons": ["Brian Harman" ,"Xander Schauffele" ,"Ryan Moore" ,"Smylie Kaufman" ,"Patrick Rodgers" ,"Chesson Hadley" ,"Bud Cauley" ,"Talor Gooch" ,"Sergio Garcia" ,"Phil Mickelson" ,"Si Woo Kim" ,"Bryson DeChambeau" ,"Ollie Schniederjans" ,"Cody Gribble" ,"Andrew Johnston" ,"Aaron Wise"],
+"Iron Spiders": ["Matt Kuchar" ,"Xander Schauffele" ,"Patrick Cantlay" ,"Chez Reavie" ,"Ollie Schniederjans" ,"Peter Uihlein" ,"Beau Hossler" ,"Anirban Lahiri" ,"Rickie Fowler" ,"Webb Simpson" ,"Keegan Bradley" ,"Emiliano Grillo" ,"Blayne Barber" ,"Andrew Loupe" ,"Mark Hubbard" ,"Y.E. Yang"],
+"Three Putt Juan": ["Dustin Johnson" ,"James Hahn" ,"Emiliano Grillo" ,"Ollie Schniederjans" ,"Chesson Hadley" ,"Brice Garnett" ,"Andrew Landry" ,"Sam Ryder" ,"Jon Rahm" ,"Patrick Reed" ,"Tony Finau" ,"Chesson Hadley" ,"Grayson Murray" ,"Tom Lovelady" ,"Keith Mitchell" ,"Sam Ryder"],
+"We`re All F*%ked": ["Hideki Matsuyama" ,"Rickie Fowler" ,"Sangmoon Bae" ,"Cheng Tsung Pan" ,"Tiger Woods" ,"Zecheng Dou" ,"Andrew Yun" ,"Xinjun Zhang" ,"Adam Scott" ,"Adam Hadwin" ,"Adam Long" ,"Adam Schenk" ,"Blake Adams" ,"Ricky Barnes" ,"Kiradech Aphibarnrat" ,"Henrik Stenson"],
+"The Rookie Can`t Get Right": ["Bubba Watson" ,"Henrik Stenson" ,"Xander Schauffele" ,"Grayson Murray" ,"K.J. Choi" ,"Jonathan Randolph" ,"David Toms" ,"Tiger Woods" ,"Sergio Garcia" ,"Bill Haas" ,"Jim Furyk" ,"Tommy Fleetwood" ,"Ernie Els" ,"K.J. Choi" ,"Davis Love III" ,"Jeff Overton"],
+"DeLaet`s Try This Wine Again": ["Rickie Fowler" ,"Rafa Cabrera Bello" ,"Francesco Molinari" ,"Thomas Pieters" ,"Alexander Noren" ,"Bud Cauley" ,"Cameron Smith" ,"Stephan Jaeger" ,"Jordan Spieth" ,"Si Woo Kim" ,"Graham DeLaet" ,"Thomas Pieters" ,"Nick Watney" ,"Sam Ryder" ,"Stephan Jaeger" ,"Aaron Wise"],
+"Carolina Pony Express": ["Dustin Johnson" ,"Patrick Reed" ,"Chez Reavie" ,"Harold Varner III" ,"Alex Cejka" ,"Greg Chalmers" ,"Ryo Ishikawa" ,"Bud Cauley" ,"Jordan Spieth" ,"Bryson DeChambeau" ,"Patrick Cantlay" ,"Thomas Pieters" ,"Kelly Kraft" ,"Colt Knost" ,"Bud Cauley" ,"Beau Hossler"],
+"Hawks Bars": ["Rory McIlroy" ,"Tony Finau" ,"Ollie Schniederjans" ,"Patrick Cantlay" ,"Thomas Pieters" ,"Peter Uihlein" ,"Corey Conners" ,"Aaron Wise" ,"Rickie Fowler" ,"Bryson DeChambeau" ,"Emiliano Grillo" ,"Ollie Schniederjans" ,"Graham DeLaet" ,"Cameron Tringale" ,"Tyrrell Hatton" ,"Stephan Jaeger"],
+"Chubbs` Right Hand in White Pants": ["Justin Thomas" ,"Keegan Bradley" ,"Martin Kaymer" ,"Danny Willett" ,"Hunter Mahan" ,"Boo Weekley" ,"K.J. Choi" ,"Austin Cook" ,"Rory McIlroy" ,"Paul Casey" ,"Sung Kang" ,"Anirban Lahiri" ,"Alexander Noren" ,"Peter Uihlein" ,"Aaron Wise" ,"Andrew Putnam"],
+"WPGJim 4.0": ["Justin Thomas" ,"Wesley Bryan" ,"Ollie Schniederjans" ,"Thomas Pieters" ,"Cheng Tsung Pan" ,"Matthew Fitzpatrick" ,"Austin Cook" ,"Abraham Ancer" ,"Rickie Fowler" ,"Patrick Reed" ,"Branden Grace" ,"Patrick Cantlay" ,"Tyrrell Hatton" ,"Brandon Hagy" ,"Bud Cauley" ,"Sam Ryder"],
+"Tink Trunk": ["Kevin Kisner" ,"Pat Perez" ,"Gary Woodland" ,"Cameron Tringale" ,"Martin Flores" ,"Bud Cauley" ,"Andrew Putnam" ,"Charlie Wi" ,"Jon Rahm" ,"Patrick Reed" ,"Emiliano Grillo" ,"Patrick Cantlay" ,"Byeong Hun An" ,"Ross Fisher" ,"Bud Cauley" ,"Sam Ryder"],
+"Numbers Under The Radar": ["Rickie Fowler" ,"Patrick Reed" ,"Ollie Schniederjans" ,"Sangmoon Bae" ,"Patrick Rodgers" ,"Richy Werenski" ,"Matthew Fitzpatrick" ,"Maverick McNealy" ,"Paul Casey" ,"Phil Mickelson" ,"Francesco Molinari" ,"Patrick Cantlay" ,"Chez Reavie" ,"Stewart Cink" ,"Jason Kokrak" ,"Bud Cauley"],
+"Blue Steele Domers": ["Jon Rahm" ,"Tony Finau" ,"J.B. Holmes" ,"Patrick Cantlay" ,"Tyrrell Hatton" ,"Harold Varner III" ,"Jason Bohn" ,"Bud Cauley" ,"Daniel Berger" ,"Brendan Steele" ,"Emiliano Grillo" ,"Patrick Cantlay" ,"Tommy Fleetwood" ,"Jamie Lovemark" ,"Seamus Power" ,"Bud Cauley"],
+"Clevelunder Kings County": ["Paul Casey" ,"Jhonattan Vegas" ,"Tony Finau" ,"Ollie Schniederjans" ,"Patrick Cantlay" ,"Patrick Rodgers" ,"Jonathan Randolph" ,"Bud Cauley" ,"Marc Leishman" ,"Xander Schauffele" ,"Sung Kang" ,"Patrick Cantlay" ,"Grayson Murray" ,"Kevin Tway" ,"Richy Werenski" ,"Seamus Power"],
+"877 FADE NOW": ["Rory McIlroy" ,"Tony Finau" ,"Phil Mickelson" ,"Ollie Schniederjans" ,"Peter Malnati" ,"Bud Cauley" ,"Aaron Wise" ,"Beau Hossler" ,"Rory McIlroy" ,"Rickie Fowler" ,"Patrick Cantlay" ,"Peter Uihlein" ,"Bud Cauley" ,"Aaron Wise" ,"Sam Ryder" ,"Talor Gooch"],
+"Nate`s A Great Rascal": ["Jordan Spieth" ,"Jon Rahm" ,"Patton Kizzire" ,"JT Poston" ,"Seamus Power" ,"Cameron Percy" ,"Bud Cauley" ,"Tag Ridings" ,"Hideki Matsuyama" ,"Kevin Kisner" ,"Patrick Cantlay" ,"Brice Garnett" ,"Peter Uihlein" ,"Abraham Ancer" ,"Bronson Burgoon" ,"Tyler Duncan"]
+};
+
+
+
+var processedTeams = Object.keys(teams).map(function(team) {
+  return {teamName: team, playerNames: teams[team], players: []}
+});
+
+
+
+
+
+
+// On ready do the magic!
+$(function() {
+  checkForData();
+  window.setInterval(function(){
+    checkForData();
+  }, 20000);
+
+});
+
+
+
+function SortByTeamTotal(a, b){
+  return ((a.teamTotal.value() > b.teamTotal.value()) ? -1 : ((a.teamTotal.value() < b.teamTotal.value()) ? 1 : 0));
+}
+
+
+
+
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+
+}
+// JSON stuff
+
+currentData = {};
+
+
+
+function checkForData() {
+
+  //$.getJSON("https://statdata.pgatour.com/r/041/2018/leaderboard-v2.json", function( data ) {
+  $.getJSON("https://statdata.pgatour.com/r/018/2018/teamleaderboard-v2.json", function( data ) {
+    
+    if (! _.isEqual(data, currentData))
+    {
+      $(".tournament-name").html("");
+      $(".panel-group").html("");
+      currentData = data;
+      teams = [];
+      
+      $.each( processedTeams, function(i, team){
+          team.players = [];
+          team.teamTotal = null;
+      })
+      // put the players' data into a local array of objects
+      
+      $.each( data.leaderboard.teams, function(i, team){
+        
+        teams.push(
+          {
+            teamName: team.player_bio.last_name,
+            player0: toTitleCase(correctedName(team.teamPlayers[0].firstName + ' ' + team.teamPlayers[0].lastName)),
+            player1: toTitleCase(correctedName(team.teamPlayers[1].firstName + ' ' + team.teamPlayers[1].lastName)),
+            positionNum: parseInt(team.current_position.replace(/\D/g,'')),
+            position: team.current_position,
+            today: team.today,
+            thru: team.thru,
+            total: team.total
+          }
+        )
+      });
+
+      $.each (processedTeams, function(i, processedTeam) {
+        capitalized = processedTeam.playerNames.map(function(name){return name.toUpperCase()});
+        
+        $.each (capitalized, function(j, playerName) {
+          
+          $.each (teams, function(k, team) {
+
+            if (team.player0.toUpperCase() === playerName)
+            {
+              processedTeam.players.push(team); 
+              
+            }
+
+            if (team.player1.toUpperCase() === playerName)
+            {
+              processedTeam.players.push(team); 
+              
+            }
+
+          });
+        });
+      });
+
+      payoutWeights = [null,18,10.8,6.8,4.8,4,3.6,3.35,3.1,2.9,2.7,2.5,2.3,2.1,1.9,1.8,1.7,1.6,1.5,1.4,1.3,1.2,1.12,1.04,0.96,0.88,0.8,0.77,0.74,0.71,0.68,0.65,0.62,0.59,0.565,0.54,0.515,0.49,0.47,0.45,0.43,0.41,0.39,0.37,0.35,0.33,0.31,0.29,0.274,0.26,0.252,0.246,0.24,0.236,0.232,0.23,0.228,0.226,0.224,0.222,0.22,0.218,0.216,0.214,0.212,0.21,0.208,0.206,0.204,0.202,0.2, 0.198, 0.196, 0.194, 0.192, 0.190, 0.188, 0.186, 0.184, 0.182, 0.180, 0.178, 0.176, 0.174, 0.172, 0.170, 0.168, 0.166]
+      
+      $.each (processedTeams, function (i, processedTeam) {
+        sum = 0;
+        $.each (processedTeam.players, function(j, player) {
+          if (! isNaN(player.positionNum))
+          {
+          	if (player.positionNum > payoutWeights.length)
+          	{
+          		sum += 0
+          	}
+          	else
+          	{
+          		sum += payoutWeights[player.positionNum]
+
+	            if (! player.thru) { player.thru = "" };
+	            if (player.today === "" || player.today === null)
+	            {
+	              player.today = ""
+	            }
+	            if (player.today === 0)
+	            {
+	              player.today = "E"
+	            }
+
+	            if (player.total == 0)
+	            {
+	              player.total = "E"
+	            }
+          	}
+            
+
+
+          }
+        });
+        
+        processedTeam.teamTotal = numeral(sum);
+ 
+      });
+      
+
+      
+      /*
+      $.each(processedTeams, function(i, team) {
+        sum = 0;
+        $.each(players, function(j, player) {
+          //Name check
+          //if (player.name.includes("Noren"))
+          //{
+            //debugger;
+          //}
+
+          capitalized = team.playerNames.map(function(name){return name.toUpperCase()});
+          if (capitalized.includes(player.name.toUpperCase()))
+          {
+            team.players.push(player);
+          }
+        });
+       
+        $.each(team.players, function(j, player) {
+          sum += player.proj_money.value();
+
+          if (! player.thru) { player.thru = "" };
+          if (player.today === "" || player.today === null)
+          {
+            player.today = ""
+          }
+          if (player.today === 0)
+          {
+            player.today = "E"
+          }
+
+          if (player.total == 0)
+          {
+            player.total = "E"
+          }
+
+
+        });
+        team.teamTotal = numeral(sum);
+      });
+      */
+
+      //Sort
+
+      processedTeams = processedTeams.sort(SortByTeamTotal);
+
+      $.each( processedTeams, function(i,processedTeam) {
+        processedTeam.players.sort(function(a,b) {
+          if(isNaN(a.positionNum - b.positionNum))
+          {
+            return !isNaN(a.positionNum) ? -1 : 1;
+          }
+          return a.positionNum - b.positionNum;
+
+        });
+      });
+
+
+      $.each(processedTeams, function(i, team) {
+          htmlString = "<div class='panel panel-default'>"
+                    + "<a class='click-track' data-toggle='collapse' href='#collapse" + i+1 + "'>"
+                    + "<div class='panel-heading'>"
+                    + "<h4 class='panel-title'>"
+                    + "<span class='pull-left label label-default'>" + (i+1) + "</span>"
+                    + "<span class='team-name'>" + team.teamName
+                    +" (" + team.players.length + ")"
+                    + "</span>"
+                    + "<span class='pull-right label label-default'>" + numeral((team.teamTotal.value() / 100) * 7200000).format("$0,0") + "</span>"
+                    + "</h4></div></a>"
+                    + "<div id='collapse" + i+1 + "' class='panel-collapse collapse'><table class='table-bordered table-condensed'>"
+                    + "<thead><tr>"
+                    + "<th>Golfer</th>"
+                    //+ "<th>Proj. $</th>"
+                    + "<th>Pos</th>"
+                    + "<th>Total</th>"
+                    + "<th>Today</th>"
+                    + "<th>Thru</th>"
+                    + "</tr></thead>";
+
+          $.each(team.players, function(index, player) {
+            //debugger;
+            htmlString += "<tr width='100%'>"
+                        + "<td>" + player.teamName + "</td>"
+                        //+ "<td>" + player.proj_money.format('$0,0') + "</td>"
+                        + "<td>" + player.position + "</td>"
+                        + "<td>" + player.total + "</td>"
+                        + "<td>" + player.today + "</td>"
+                        + "<td>" + player.thru + "</td>"
+                        + "</tr>"
+
+          });
+                    //+
+                    htmlString+= "</table></ul></div></div>";
+          $(".panel-group").append(htmlString);
+      });
+
+      $(".tournament-name").append("<h1>" + data.leaderboard.tournament_name + "</h1>");
+      //$(".tournament-name").append("<h1>" + "WGC-HSBC/Sanderson Farms" + "</h1>");
+
+      $(".click-track").click(function(){
+          ga('send', 'event', 'Teams', 'Click', $(this).find(".team-name").text());
+      });
+    }
+    else
+    {
+      console.log("Data hasn't changed!");
+    }
+    $(".error-message").hide();
+  })
+}
+
+
+
+function correctedName(name) {
+  if (name == "Siwoo Kim")
+  {
+    return "Si Woo Kim";
+  }
+  else if (name == "Sunghoon Kang")
+  {
+    return "Sung Kang";
+  }
+  else if (name == "Byeonghun An")
+  {
+    return "Byeong Hun An";
+  }
+  else if (name == "Byeong-Hun An")
+  {
+    return "Byeong Hun An";
+  }
+  else if (name == "Shih-chang Chan")
+  {
+    return "Chan Shih-chang";
+  }
+  else if (name == "S.S.P Chawrasia")
+  {
+    return "SSP Chawrasia";
+  }
+  else if (name == "Soomin Lee")
+  {
+    return "Lee Soo-Min";
+  }
+  else if (name == "Younghan Song")
+  {
+    return "Young-han Song";
+  }
+  else if (name == "Alex Noren")
+  {
+    return "Alexander Noren";
+  }
+  else if (name == "J.T. Poston")
+  {
+    return "JT Poston";
+  }
+  else if (name == "C.T. Pan")
+  {
+    return "Cheng Tsung Pan";
+  }
+  else if (name == "Willy Wilcox")
+  {
+    return "Will Wilcox";
+  }
+  else if (name == "Jeung-hun Wang")
+  {
+    return "Jeunghun Wang";
+  }
+  else if (name == "Thorbjørn Olesen")
+  {
+    return "Thorbjorn Olesen";
+  }
+  else if (name == "Sebastián Muñoz")
+  {
+    return "Sebastian Munoz";
+  }
+  else if (name == "Ángel Cabrera")
+  {
+    return "Angel Cabrera";
+  }
+  else if (name == "Julián Etulain")
+  {
+    return "Julian Etulain";
+  }
+  else if (name == "Søren Kjeldsen")
+  {
+    return "Soren Kjeldsen";
+  }
+  else if (name == "Fabián Gómez")
+  {
+    return "Fabian Gomez";
+  }
+  else if (name == "Roberto Díaz")
+  {
+    return "Roberto Diaz";
+  }
+  else if (name == "Richard T. lee")
+  {
+    return "Richard Lee";
+  }
+  else if (name == "C. T. Pan")
+  {
+    return "Cheng Tsung Pan";
+  }
+  else if (name == "C. T. Pan")
+  {
+    return "Cheng Tsung Pan";
+  }
+  else if (name == "Hao Tong Li")
+  {
+    return "Haotong Li";
+  }
+  else if (name == "Rodney Pampling")
+  {
+    return "Rod Pampling";
+  }
+  else{return name;}
+
+}
