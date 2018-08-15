bf6204f33e0a6ef4214381cc9d044aa77fee61cc
mhartman4 <mhartman4@gmail.com>
2018-08-15 12:13:53 -0400

    error

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 
 function getRedSoxRecord() {
-  $.getJSON('http://anyorigin.com/go?url=https%3A//www.baseball-reference.com/teams/BOS/2018.shtml&callback=?.html', function(data){
+  $.getJSON('http://anyorigin.com/go?url=https%3A//www.baseball-reference.com/teams/BOS/2018.shtml&callback=?', function(data){
+    
     return data.contents;
   });
 }
