69a1e5025314ac418608abf310a73a4c0f717d0c
mhartman4 <mhartman4@gmail.com>
2018-08-15 12:21:31 -0400

    think i got it

--- test.js
+++ test.js
@@ -2,7 +2,11 @@
 function getRedSoxRecord() {
   $.getJSON('http://anyorigin.com/go?url=https%3A//www.baseball-reference.com/teams/BOS/2018.shtml&callback=?', function(data){
     
-    return data.contents;
+    blob = data.contents.substring(data.contents.search("<strong>Record:</strong>"), data.contents.search("<strong>Record:</strong>")+50);
+    blob = blob.substring(0, blob.search(","));
+    blob = blob.replace("<strong>Record:</strong>", "").trim()
+    return blob;
+
   });
 }
 
