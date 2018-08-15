d11a69e62eca40254ebcda69268be4125526d0e1
mhartman4 <mhartman4@gmail.com>
2018-08-15 11:47:01 -0400

    wrong url

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
 function getRedSoxRecord() {
-  $.getJSON('baseballref.html', function(data){
+  $.getJSON('http://anyorigin.com/go?url=https%3A//www.baseball-reference.com/teams/BOS/2018.shtml&callback=?.html', function(data){
     return data.contents;
   });
 }
