ba86ac1fd9dfb31d67bb324496f4b01d3edf0157
Michael Hartman <mhartman4@gmail,com>
2020-01-08 12:33:34 -0500

    get rid of the console log

--- newDesignOverall2.js
+++ newDesignOverall2.js
@@ -79,11 +79,7 @@ teamNamesAndOwners = {
 
 $(function() {
   jQuery.ajaxSetup({async:false});
-  console.log("Load the javascript file here;")
   filltheTable();
-
-
-
 });
 
 function filltheTable() {
