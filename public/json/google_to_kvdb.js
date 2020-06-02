003e5c1450a713503a9964cc98896fdf0261bafa
Michael Hartman <mhartman4@gmail,com>
2020-06-02 17:42:06 -0400

    change the overall one to be KVDB too to speed things up

--- public/json/google_to_kvdb.js
+++ public/json/google_to_kvdb.js
@@ -13,3 +13,9 @@ request("https://spreadsheets.google.com/feeds/list/1YsZn_ovmbxOE8gUlmAT7z_nUv5m
   const bucket = KVdb.bucket("vRrcDLPTr4WWpVTJxim1H");
   bucket.set('rosters', body);
 });
+
+// Overall
+request("https://spreadsheets.google.com/feeds/list/1YsZn_ovmbxOE8gUlmAT7z_nUv5mg9qRdwnNAX-lIrnI/3/public/full?alt=json", function(error, response, body) {
+  const bucket = KVdb.bucket("vRrcDLPTr4WWpVTJxim1H");
+  bucket.set('overall', body);
+});
